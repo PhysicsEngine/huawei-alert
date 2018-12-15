5e3a17d33cd3149f0b6e9d54f150704d32477ed0
ksgwr <ksgwarai@gmail.com>
2018-12-15 15:02:22 +0900

    init project

---
+++ HuaweiAlert/HuaweiAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HuaweiAlert
+//
+//  Created by 菅原晃平 on 2018/12/15.
+//  Copyright © 2018年 菅原晃平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
